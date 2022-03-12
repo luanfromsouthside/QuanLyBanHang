d3a5a33ac69aae5f11af1f8873c08ad3c4997f55
luanwestside <justin.henry.gd@gmail.com>
2022-03-12 12:59:30 +0700

    add model student

---
+++ QuanLyBanHang/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyBanHang.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Born { get; set; }
+        public string Class { get; set; }
+        public string Specialized { get; set; } 
+    }
+}
