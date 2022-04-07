ce8237b6b7c72a0bbc0d76b13e5cb4cfe182f0b9
luanwestside <justin.henry.gd@gmail.com>
2022-04-07 20:03:06 +0700

    lab5

---
+++ QuanLyBanHang/Controllers/FacultiesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyBanHang.Models;
+
+namespace QuanLyBanHang.Controllers
+{
+    public class FacultiesController : Controller
+    {
+        private TestDataEntities db = new TestDataEntities();
+
+        // GET: Faculties
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Faculties.ToListAsync());
+        }
+
+        // GET: Faculties/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faculty faculty = await db.Faculties.FindAsync(id);
+            if (faculty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faculty);
+        }
+
+        // GET: Faculties/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Faculties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "FacultyId,FacultyName")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Faculties.Add(faculty);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(faculty);
+        }
+
+        // GET: Faculties/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faculty faculty = await db.Faculties.FindAsync(id);
+            if (faculty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faculty);
+        }
+
+        // POST: Faculties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "FacultyId,FacultyName")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(faculty).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(faculty);
+        }
+
+        // GET: Faculties/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faculty faculty = await db.Faculties.FindAsync(id);
+            if (faculty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faculty);
+        }
+
+        // POST: Faculties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Faculty faculty = await db.Faculties.FindAsync(id);
+            db.Faculties.Remove(faculty);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
